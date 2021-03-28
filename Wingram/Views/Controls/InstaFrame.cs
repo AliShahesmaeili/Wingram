7d84b58b88e91ba6dd7c0da0322957b80e24a29a
AliShahesmaeili <shimash.loves@gmail.com>
2021-03-29 01:25:55 +0430

    Added transition effects when InstaPage is navigating

---
+++ Wingram/Views/Controls/InstaFrame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+
+namespace Wingram.Views.Controls
+{
+    public class InstaFrame : Frame
+    {
+        public InstaFrame()
+        {
+          
+        }
+    }
+}
--- Wingram/Views/Controls/InstaPage.cs
+++ Wingram/Views/Controls/InstaPage.cs
@@ -1,15 +1,24 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf.Transitions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Wingram.Views.Controls
 {
     public class InstaPage : Page
     {
+        public static DependencyProperty ContentPageProperty =
+    DependencyProperty.Register("ContentPage", typeof(object), typeof(InstaPage));
+        public object ContentPage
+        {
+            get => GetValue(ContentPageProperty);
+            set => SetValue(ContentPageProperty, value);
+        }
         public InstaPage()
         {
             if (!DesignerProperties.GetIsInDesignMode(this))
@@ -17,6 +26,17 @@ namespace Wingram.Views.Controls
                 FlowDirection = Classes.Constants.ApplicationFlowDirection;
                 FontFamily = Classes.Constants.ApplicationFontFamily;
             }
+            Loaded += InstaPage_Loaded;
+        }
+
+        private void InstaPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var transitioningContent = new TransitioningContent();
+            transitioningContent.Content = ContentPage;
+            transitioningContent.RunHint = TransitioningContentRunHint.All;
+            transitioningContent.OpeningEffects.Add(new TransitionEffect(TransitionEffectKind.FadeIn));
+            transitioningContent.OpeningEffects.Add(new TransitionEffect(TransitionEffectKind.SlideInFromLeft));
+            Content = transitioningContent;
         }
     }
 }
