8871073af97a37451b77e84f8e2dda6b7116ab6c
AliShahesmaeili <shimash.loves@gmail.com>
2021-03-30 21:16:06 +0430

    Code cleanup

--- Wingram/Views/LoginPage.xaml.cs
+++ Wingram/Views/LoginPage.xaml.cs
@@ -1,19 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Wingram.Classes.ViewModels;
-using Wingram.Enums;
 using Wingram.Views.Controls;
 using Wingram.Views.Pages;
 
@@ -24,24 +12,18 @@ namespace Wingram.Views
     /// </summary>
     public partial class LoginPage : InstaPage
     {
-        LoginViewModel loginViewModel => DataContext as LoginViewModel;
+        LoginViewModel LoginViewModel => DataContext as LoginViewModel;
         public LoginPage()
         {
             InitializeComponent();
         }
-        public override void PageLoad(PageLoadEnum pageLoadEnum, Dictionary<string, object> parameters)
-        {
-
-        }
         private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
-            loginViewModel.Password = ((PasswordBox)sender).Password;
+            LoginViewModel.Password = ((PasswordBox)sender).Password;
         }
-
         private void TextBlockLoginHelp_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            loginViewModel.ApplicationViewModel.Navigate(typeof(LoginHelpPage), false, new Dictionary<string, object>() { { "asdad", 1 } });
-            // NavigationService.Navigate(new LoginPage());
+            LoginViewModel.ApplicationViewModel.Navigate(typeof(LoginHelpPage), false);
         }
     }
 }
